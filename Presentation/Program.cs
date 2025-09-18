ae6facc615b929ada92d86fa849b26fb59456d67
Kstadhammer <109215347+Kstadhammer@users.noreply.github.com>
2025-09-18 16:45:57 +0200

    Updated Program.cs with Swagger

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,18 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi.Models;
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+// Move these BEFORE app.Build()
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        }
+    );
+});
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "InstructorService", Version = "v1" });
+});
+
+var app = builder.Build();
 
 app.MapOpenApi();
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll"); // Add this line
 app.UseAuthorization();
 app.UseAuthentication();
-
 app.MapControllers();
 
 app.Run();
